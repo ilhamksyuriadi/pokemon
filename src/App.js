1d9770ff6fb6491424a042acc7cdba8941ac523d
miki <ilhamksyuriadi@gmail.com>
2020-11-15 06:12:02 +0700

    done styling list pokemon

--- src/App.js
+++ src/App.js
@@ -1,33 +1,11 @@
 import './App.css';
-
-import { useQuery, gql } from '@apollo/client';
-
-const ALL_POKEMON = gql`
-    query pokemon($name: String!) {
-      pokemon(name: $name) {
-        id
-        name
-        message
-        status
-      }
-    }
-`
+import PokemonList from './views/PokemonList';
 
 function App() {
-  const {loading, error, data} = useQuery(ALL_POKEMON,
-    {
-      variables: {
-        name: "ditto"
-      }
-    }
-  )
-
-  if (loading) return <p>Loading...</p>
-  if (error) return `Error! ${error}`
   return (
-    <>
-      <h2>{data.pokemon.name}</h2>
-    </>
+    <div className="App">
+      <PokemonList />
+    </div>
   );
 }
 
