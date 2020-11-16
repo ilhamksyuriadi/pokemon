98d9b2147ce3de21cacfc426a89c639d1f168c09
miki <ilhamksyuriadi@gmail.com>
2020-11-17 01:30:16 +0700

    adding and styling catch entry point

--- src/views/PokemonDetail.js
+++ src/views/PokemonDetail.js
@@ -1,18 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from '@apollo/client';
 import { GET_POKEMON_DETAIL } from '../operations/queries/getPokemon';
 import { useParams } from 'react-router-dom';
 import './PokemonDetail.css';
 import Loading from '../components/Loading';
 import Error from '../components/Error';
+import PokeBall from '../assets/pokeball.png';
 
 const PokemonDetail = () => {
     const { name } = useParams();
     const variables = { name }
-    const {loading, error, data} = useQuery(GET_POKEMON_DETAIL, { variables })
+    const { loading, error, data } = useQuery(GET_POKEMON_DETAIL, { variables });
+    const [ catchState, setCatchState ] = useState('standby')
+
+    // useEffect(()=>{
+    //     console.log('useffect')
+    // },[catchState])
+
+    const getCatchChance = () => {
+        return Math.floor(Math.random() * 2);
+    }
 
     const handleCatch = () => {
         console.log('catch clicked')
+        setCatchState('catching')
+        const chance = getCatchChance()
+        if (chance >= 1) {
+            setTimeout(()=>{
+                console.log('success', chance)
+                setCatchState('success')
+            },5000)
+        } else {
+            setTimeout(()=>{
+                console.log('fail', chance)
+                setCatchState('fail')
+            },5000)
+        }
+        console.log(catchState)
     }
 
     if (loading) return <Loading msg='getting pokemon detail...' />
@@ -34,7 +58,8 @@ const PokemonDetail = () => {
         const pokemonAbilities = listAbilities.join(', ')
         const pokemonMoves = listMoves.join(', ')
         return (
-            <>
+            <> 
+                <h1>test</h1>
                 <h2>Pokemon Detail</h2>
                 <div className="detail-layout">
                     <div className="profile-box">
@@ -45,9 +70,15 @@ const PokemonDetail = () => {
                         <h4 className="pokemon-text">pokemon id: {pokemon.id}</h4>
                         <h4 className="pokemon-text">type:  {listTypes}</h4>
                         <h4 className="pokemon-text">owned: 0</h4>
-                        <button onClick={handleCatch} className="catch-button">
-                            catch
-                        </button>
+                        <div className={`catch-wrapper wrapper-${catchState}`}>
+                            <code>{catchState}</code>
+                            <div className="box-wrapper">
+                                <button onClick={handleCatch} className={"catch-button"}>
+                                    catch
+                                </button>
+                                <img className={`pokeball pokeball-${catchState}`} src={PokeBall} alt="pokeball"></img>
+                            </div>
+                        </div>
                     </div>
                     <div className="description-box">
                         <div className="abilities-box">
